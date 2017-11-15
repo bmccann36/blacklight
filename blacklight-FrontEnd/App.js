c3dc67466f20d79b9add8898c03d2762222146c1
Alexander Tatarinov <tatarinov.alexander@gmail.com>
2017-11-15 12:09:14 -0500

    initialized the front-end

--- blacklight-FrontEnd/App.js
+++ blacklight-FrontEnd/App.js
@@ -5,7 +5,9 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>THIS IS THE FIRST PAGE</Text>
+        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Changes you make will automatically reload.</Text>
+        <Text>Shake your phone to open the developer menu.</Text>
       </View>
     );
   }
