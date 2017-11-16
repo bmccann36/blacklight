b8427b7af067750558956572d756e4f283fba660
Trevor Storey <trevorstorey23@gmail.com>
2017-11-16 11:43:00 -0500

    [Feature], post/create route works through postman. moving to put/update route

--- server/api/memories.js
+++ server/api/memories.js
@@ -8,3 +8,15 @@ router.get('/', (req, res, next) => {
     .then(memories => res.json(memories))
     .catch(next);
 });
+
+router.post('/', (req, res, next) => {
+  Memory.create(req.body)
+    .then(createdMemory => res.json(createdMemory))
+    .catch(next);
+});
+
+router.put('/:memoryId', (req, res, next) => {
+  Memory.update({ where: { id: req.params.memoryId } }, req.body)
+    .then(updatedMemory => res.json(updatedMemory))
+    .catch(next);
+});
