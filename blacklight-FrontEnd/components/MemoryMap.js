27bccac04a2bb8d691cf7eecfe8c80227800f73e
MINAMI K <minamik@MINAMIKs-MacBook-Air.local>
2017-11-17 18:45:07 -0500

    marker created

--- blacklight-FrontEnd/components/MemoryMap.js
+++ blacklight-FrontEnd/components/MemoryMap.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, Dimensions } from 'react-native';
 import { StackNavigator } from 'react-navigation';
 import { connect } from 'react-redux';
 import MapView from 'react-native-maps';
@@ -8,24 +8,32 @@ import FrontPage from './FrontPage';
 import MemoryList from './MemoryList';
 
 export default class MemoryMap extends Component {
-
-
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>working</Text>
-        <MapView style={styles.map}
-          initialRegion={{
-            latitude: 37.78825,
-            longitude: -122.4324,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421,
-          }}
-        />
-      </View>
-    );
-  }
+render() {
+  return (
+    <View style={styles.container}>
+      <MapView
+      style={styles.map}
+        initialRegion={{
+          latitude: 40.705076,
+          longitude: -74.0113487,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}>
+        <MapView.Marker
+          coordinate={{
+            latitude: 40.705076,
+            longitude: -74.0113487,
+          }}>
+          <View style={styles.radius}>
+            <View style={styles.marker}/>
+          </View>
+        </MapView.Marker>
+      </MapView>
+    </View>
+  );
 }
+}
+
 
 const styles = StyleSheet.create({
   container: {
@@ -40,6 +48,112 @@ const styles = StyleSheet.create({
     left:0,
     right:0,
     bottom:0,
+  },
+  radius:{
+    height:50,
+    width:50,
+    borderRadius: 50/2,
+    overflow: 'hidden',
+    backgroundColor: 'rgba(0,122,255,0.1)',
+    borderWidth: 1,
+    borderColor: 'rgba(0, 122, 255, 0.3)',
+    alignItems:'center',
+    justifyContent: 'center'
+  },
+  marker:{
+    height:20,
+    width:20,
+    borderWidth: 3,
+    borderColor: 'white',
+    borderRadius: 20 / 2,
+    overflow: 'hidden',
+    backgroundColor: '#007AFF',
   }
 });
 
+
+
+// const {width, height} = Dimensions.get('window')
+
+// const SCREEN_HEIGHT = height
+// const SCREEN_WIDTH = width
+// const ASPECT_RATIO = width / height
+// const LATTITUDE_DELTA = 0.0922
+// const LONGTITUDE_DELTA = LATTITUDE_DELTA * ASPECT_RATIO
+
+
+// export default class MemoryMap extends Component {
+//   constructor(props){
+//     super(props)
+
+//     this.state = {
+//       initialPosition:{
+//         latitude: 0,
+//         longitude: 0,
+//         latitudeDelta: 0,
+//         longitudeDelta: 0
+//       },
+//       markerPosition:{
+//         latitude: 0,
+//         longitude: 0
+//       }
+//     }
+//   }
+
+//   watchID: ?number = null
+
+//   componentDidMount(){
+//     navigator.geolocation.getCurrentPosition((position) => {
+//       let lat = parseFloat(position.coords.latitude)
+//       let long = parseFloat(position.coords.longtitude)
+
+//       let initialRegion = {
+//         latitude: lat,
+//         longitude: long,
+//         latitudeDelta: LATTITUDE_DELTA,
+//         longitudeDelta: LONGTITUDE_DELTA
+//       }
+//       this.setState({initialPosition: initialRegion})
+//       this.setState({markerPosition: initialRegion})
+//     },
+//     (error) => alert(JSON.stringify(error)),
+//     {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000})
+
+//     this.watchID = navigator.geolocation.watchPosition((position) => {
+//       let lat = parseFloat(position.coords.latitude)
+//       let long = parseFloat(position.coords.longtitude)
+
+//       let lastRegion = {
+//         latitude: lat,
+//         longitude: long,
+//         latitudeDelta: LATTITUDE_DELTA,
+//         longitudeDelta: LONGTITUDE_DELTA
+//       }
+//       this.setState({initialPosition: lastRegion})
+//       this.setState({markerPosition: lastRegion})
+//     })
+//   }
+
+//   componentWillUnmount(){
+//     navigator.geolocation.clearWatch(this.watchID)
+//   }
+
+
+//   render() {
+//     return (
+//       <View style={styles.container}>
+//         <MapView
+//         style={styles.map}
+//           Region={this.state.initialPosition}>
+//           <MapView.Marker
+//             coordinate={this.state.markerPosition}>
+//             <View style={styles.radius}>
+//               <View style={styles.marker}/>
+//             </View>
+//           </MapView.Marker>
+//         </MapView>
+//       </View>
+//     );
+//   }
+// }
+// }
