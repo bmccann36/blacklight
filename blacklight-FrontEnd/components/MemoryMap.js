434d665e35728fa5728890a6f3a656348383fb9c
Alexander Tatarinov <tatarinov.alexander@gmail.com>
2017-11-19 18:43:42 -0500

    added users location

--- blacklight-FrontEnd/components/MemoryMap.js
+++ blacklight-FrontEnd/components/MemoryMap.js
@@ -4,37 +4,136 @@ import { StyleSheet, View, Text, Dimensions } from 'react-native';
 import { StackNavigator } from 'react-navigation';
 import { connect } from 'react-redux';
 import MapView from 'react-native-maps';
+import store from '../store';
 
 import FrontPage from './FrontPage';
 import MemoryList from './MemoryList';
 
-export default class MemoryMap extends Component {
-render() {
-  return (
-    <View style={styles.container}>
-      <MapView
-      style={styles.map}
-        initialRegion={{
-          latitude: 40.705076,
-          longitude: -74.0113487,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}>
-        <MapView.Marker
-          coordinate={{
-            latitude: 40.705076,
-            longitude: -74.0113487,
-          }}>
-          <View style={styles.radius}>
-            <View style={styles.marker}/>
-          </View>
-        </MapView.Marker>
-      </MapView>
-    </View>
-  );
-}
-}
+// const { width, height } = Dimensions.get('window');
+// const SCREEN_HEIGHT = height;
+// const SCREEN_WIDTH = width;
+// const ASPECT_RATIO = width / height;
+// const LATTITUDE_DELTA = 0.0922;
+// const LONGTITUDE_DELTA = LATTITUDE_DELTA * ASPECT_RATIO;
 
+
+export default class MemoryMap extends Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      currentLocation: {
+        latitude: 40.705076,
+        longitude: -74.0113487,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421
+      },
+      markerPosition: {
+        latitude: 40.705076,
+        longitude: -74.0113487
+      },
+      memories: [{
+        "createdAt": "2017-11-16T18:08:27.654Z",
+        "id": 1,
+        "lat": 40.7050758,
+        "lng": -74.00916039999998,
+        "text": "pretty exciting connecting back and front",
+        "title": "the first time",
+        "updatedAt": "2017-11-16T18:08:27.654Z",
+      },
+      {
+        "createdAt": "2017-11-16T18:13:09.514Z",
+        "id": 2,
+        "lat": 40.75048649999999,
+        "lng": -73.97640100000001,
+        "text": "pretty exciting connecting back and front",
+        "title": "the second time",
+        "updatedAt": "2017-11-16T18:13:09.514Z",
+      }]
+    };
+  }
+
+  watchID = ''
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(position => {
+      this.setState({
+        currentLocation: {
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421
+        }
+      });
+      this.setState({
+        markerPosition: {
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude
+        }
+      });
+    }, (error) => alert(JSON.stringify(error)),
+      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
+    );
+
+    this.watchID = navigator.geolocation.watchPosition(position => {
+      this.setState({
+        currentLocation: {
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421
+        }
+      });
+
+      this.setState({
+        markerPosition: {
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude
+        }
+      });
+    })
+  }
+
+  componentWillUnmount() {
+    navigator.geolocation.clearWatch(this.watchID);
+  }
+
+
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <MapView
+          style={styles.map}
+          region={this.state.currentLocation}>
+          {
+            <MapView.Marker
+              coordinate={this.state.markerPosition}>
+              <View style={styles.radius}>
+                <View style={styles.marker} />
+              </View>
+            </MapView.Marker>
+          }
+
+          {
+            this.state.memories.map(mem => (
+              <MapView.Marker
+                key={mem.id}
+                coordinate={{ latitude: mem.lat, longitude: mem.lng }}
+                title={mem.title}
+              description={mem.text}
+              />
+            ))
+          }
+
+
+        </MapView>
+
+      </View>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   container: {
@@ -43,27 +142,27 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  map:{
-    position:'absolute',
-    top:0,
-    left:0,
-    right:0,
-    bottom:0,
+  map: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
   },
-  radius:{
-    height:50,
-    width:50,
-    borderRadius: 50/2,
+  radius: {
+    height: 50,
+    width: 50,
+    borderRadius: 50 / 2,
     overflow: 'hidden',
     backgroundColor: 'rgba(0,122,255,0.1)',
     borderWidth: 1,
     borderColor: 'rgba(0, 122, 255, 0.3)',
-    alignItems:'center',
+    alignItems: 'center',
     justifyContent: 'center'
   },
-  marker:{
-    height:20,
-    width:20,
+  marker: {
+    height: 20,
+    width: 20,
     borderWidth: 3,
     borderColor: 'white',
     borderRadius: 20 / 2,
@@ -72,89 +171,13 @@ const styles = StyleSheet.create({
   }
 });
 
+// const mapDispatchToProps = { fetchMemories };
+
+// const mapStateToProps = (state) => ({
+//   memories: state.memories
+// });
+
+// export default connect(mapStateToProps, mapDispatchToProps)(MemoryList);
 
 
-// const {width, height} = Dimensions.get('window')
 
-// const SCREEN_HEIGHT = height
-// const SCREEN_WIDTH = width
-// const ASPECT_RATIO = width / height
-// const LATTITUDE_DELTA = 0.0922
-// const LONGTITUDE_DELTA = LATTITUDE_DELTA * ASPECT_RATIO
-
-
-// export default class MemoryMap extends Component {
-//   constructor(props){
-//     super(props)
-
-//     this.state = {
-//       initialPosition:{
-//         latitude: 0,
-//         longitude: 0,
-//         latitudeDelta: 0,
-//         longitudeDelta: 0
-//       },
-//       markerPosition:{
-//         latitude: 0,
-//         longitude: 0
-//       }
-//     }
-//   }
-
-//   watchID: ?number = null
-
-//   componentDidMount(){
-//     navigator.geolocation.getCurrentPosition((position) => {
-//       let lat = parseFloat(position.coords.latitude)
-//       let long = parseFloat(position.coords.longtitude)
-
-//       let initialRegion = {
-//         latitude: lat,
-//         longitude: long,
-//         latitudeDelta: LATTITUDE_DELTA,
-//         longitudeDelta: LONGTITUDE_DELTA
-//       }
-//       this.setState({initialPosition: initialRegion})
-//       this.setState({markerPosition: initialRegion})
-//     },
-//     (error) => alert(JSON.stringify(error)),
-//     {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000})
-
-//     this.watchID = navigator.geolocation.watchPosition((position) => {
-//       let lat = parseFloat(position.coords.latitude)
-//       let long = parseFloat(position.coords.longtitude)
-
-//       let lastRegion = {
-//         latitude: lat,
-//         longitude: long,
-//         latitudeDelta: LATTITUDE_DELTA,
-//         longitudeDelta: LONGTITUDE_DELTA
-//       }
-//       this.setState({initialPosition: lastRegion})
-//       this.setState({markerPosition: lastRegion})
-//     })
-//   }
-
-//   componentWillUnmount(){
-//     navigator.geolocation.clearWatch(this.watchID)
-//   }
-
-
-//   render() {
-//     return (
-//       <View style={styles.container}>
-//         <MapView
-//         style={styles.map}
-//           Region={this.state.initialPosition}>
-//           <MapView.Marker
-//             coordinate={this.state.markerPosition}>
-//             <View style={styles.radius}>
-//               <View style={styles.marker}/>
-//             </View>
-//           </MapView.Marker>
-//         </MapView>
-//       </View>
-//     );
-//   }
-// }
-// }
