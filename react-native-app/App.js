6f7c10b17f91623a1b08a8fff4c681d42c3cbc23
Brian McCann <bmccann36@gmail.com>
2017-11-21 18:08:44 -0500

    moved App up a directory level, adjusted imports  in app accordingly so app renders

--- react-native-app/App.js
+++ react-native-app/App.js
@@ -3,20 +3,17 @@ import React, { Component } from 'react';
 import { StyleSheet, View } from 'react-native';
 import { StackNavigator, TabNavigator, TabRouter } from 'react-navigation';
 import { Provider } from 'react-redux';
-import store from '../store';
+import store from './store';
 import { Icon } from 'react-native-elements';
 
-import MemoryList from './MemoryList';
-import Home from './Home';
-import User from './User';
-import MemoryMap from './MemoryMap';
-import AddMemory from './AddMemory';
-import Login from './Login';
-// const MyApp = TabRouter({
-//   Home: {screen: Home},
-//   User: {screen: User}
-// })
-import SingleMemoryView from './SingleMemoryView';
+import MemoryList from './components/MemoryList';
+import Home from './components/Home';
+import User from './components/User';
+import MemoryMap from './components/MemoryMap';
+import AddMemory from './components/AddMemory';
+import Login from './components/Login';
+
+import SingleMemoryView from './components/SingleMemoryView';
 
 
 //memories stack nav config
@@ -38,59 +35,53 @@ const TabNav = TabNavigator({
     screen: Home,
     navigationOptions: {
       tabBarLabel: "Home",
-      tabBarIcon: ({ tintColor }) => <Icon name="home" size={35} color={tintColor}/>
+      tabBarIcon: ({ tintColor }) => <Icon name="home" size={35} color={tintColor} />
     }
   },
   Memories: {
     screen: Views,
     navigationOptions: {
       tabBarLabel: "Memories",
-      tabBarIcon: ({ tintColor }) => <Icon name="menu" size={35} color={tintColor}/>
+      tabBarIcon: ({ tintColor }) => <Icon name="menu" size={35} color={tintColor} />
     }
   },
   Map: {
     screen: MemoryMap,
     navigationOptions: {
       tabBarLabel: "Map",
-      tabBarIcon: ({ tintColor }) => <Icon name="map" size={35} color={tintColor}/>
+      tabBarIcon: ({ tintColor }) => <Icon name="map" size={35} color={tintColor} />
     }
   },
   Post: {
     screen: AddMemory,
     navigationOptions: {
       tabBarLabel: "Post Memory",
-      tabBarIcon: ({ tintColor }) => <Icon name="note-add" size={35} color={tintColor}/>
+      tabBarIcon: ({ tintColor }) => <Icon name="note-add" size={35} color={tintColor} />
     }
   },
   Login: { screen: Login } // can I throw this here and have it render??
 
 }, {
-  tabBarPosition: 'bottom',
-  // animationEnabled: true,
-  // tabBarOptions: {
-  //   style: {position: 'absolute', flex: 1},
-  //   activeTintColor: '#e91e63',
-  // },
-});
+    tabBarPosition: 'bottom',
+    // animationEnabled: true,
+    // tabBarOptions: {
+    //   style: {position: 'absolute', flex: 1},
+    //   activeTintColor: '#e91e63',
+    // },
+  });
 
 
 export default class App extends Component {
   render() {
-
     return (
       <Provider store={store}>
         <View style={styles.container}>
-
-
-
           <TabNav />
         </View>
       </Provider>
     );
-
   }
 }
-
 
 const styles = StyleSheet.create({
   container: {
