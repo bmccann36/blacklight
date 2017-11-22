0034c5b5ec28882a87d860047e7ac9d6fc6ba987
Trevor Storey <trevorstorey23@gmail.com>
2017-11-22 18:45:11 -0500

    [Eject] successful ejection. move to pod install

---
+++ react-native-app/android/app/src/main/java/blacklight/fs/app/generated/ExponentBuildConstants.java
@@ -0,0 +1,8 @@
+package blacklight.fs.app.generated;
+
+// This file is auto-generated. Please don't rename!
+public class ExponentBuildConstants {
+
+  public static final String DEVELOPMENT_URL = "";
+
+}
