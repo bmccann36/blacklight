31ddfedce18bdeeead7ad79bbdb28069c0e6fea3
Brian McCann <bmccann36@gmail.com>
2017-11-25 17:29:15 -0500

    [add] login form

--- react-native-app/router.js
+++ react-native-app/router.js
@@ -6,6 +6,7 @@ import { Text } from 'react-native';
 
 import Test from './components/Test'
 import Test2 from './components/Test2'
+import Login from './components/Login'
 
 // Simple component to render something in place of icon
 const TabIcon = ({ selected, title }) => {
@@ -14,14 +15,36 @@ const TabIcon = ({ selected, title }) => {
   );
 }
 
-const RouterComponent = () => {
-  return (
+const RouterComponent = props => (
   <Router sceneStyle={{ paddingTop: 30 }}>
-    <Scene key="test" component={Test} title="LANDING PAGE"/>
-    <Scene key="test2" component={Test2} title="other page" initial />
+    <Scene key="login" component={Login} title="LANDING PAGE" />
+    <Scene key="root">
+      {/* Tab Container */}
+      <Scene
+        key="tabbar"
+        tabs
+        tabBarStyle={{ backgroundColor: '#FFFFFF', height: 70 }}
+      >
+        {/* MAP WITH MEMORIES */}
+        <Scene key="key1" title="title" icon={TabIcon}>
+          <Scene
+            key="test1"
+            component={Test}
+            title="TAB 1"
+          />
+        </Scene>
+        <Scene key="key2" title="title" icon={TabIcon}>
+          <Scene
+            key="test2"
+            component={Test2}
+            title="TAB 1"
+          />
+        </Scene>
+      </Scene>
+
+    </Scene>
   </Router>
-  )
-}
+);
 
 
 export default RouterComponent;
