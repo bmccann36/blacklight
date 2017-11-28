1016e25019264b7bc0c543e3694be170e10cd7bc
Minami Kato <minamikato@gmail.com>
2017-11-27 22:23:23 -0500

    Initial Styling by Nadim

--- react-native-app/Router.js
+++ react-native-app/Router.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Scene, Router, Actions } from 'react-native-router-flux';
-import { Text } from 'react-native';
-
+// import { Text } from 'react-native';
+import { Icon, Button, Text } from 'react-native-elements'
 
 
 import Test from './components/Test';
@@ -10,50 +10,71 @@ import Record from './components/Record';
 import Main from './components/Main';
 import RecordInput from './components/RecordInput';
 import NearbyMem from './components/NearbyMem';
+
 // Simple component to render something in place of icon
-const TabIcon = ({ selected, title }) => {
-  return (
-    <Text style={{ color: selected ? 'red' : 'black' }}>{title}</Text>
-  );
-}
+const TabIcon = ({ focused, title }) => {
+  switch (title){
+    case 'MAIN' :
+      return (
+    <Icon color="#cec4c4" name="globe" size={32} type="feather" />
+      )
+    case 'REC' :
+      return (
+    <Icon color="#cec4c4" name="loupe" size={32} type="loupe" />
+      )
+    case 'NEARBY' :
+      return (
+    <Icon color="#cec4c4" name="unlock" size={32} type="feather" />
+      );
+  }
+};
+
+// <Scene key="login" component={Login} title="LANDING PAGE" />
 
 const RouterComponent = props => (
-  <Router sceneStyle={{ paddingTop: 30 }}>
-    <Scene key="login" component={Login} title="LANDING PAGE" />
+  <Router
+    // sceneStyle={{backgroundColor: 'black'}}
+    navigationBarStyle={styles.navBar}
+    titleStyle={styles.navBarTitle}
+    barButtonTextStyle={styles.barButtonTextStyle} barButtonIconStyle={styles.barButtonIconStyle}
+    >
+
+    <Scene key="login" component={Login} />
     <Scene key="root">
       {/* Tab Container */}
       <Scene
         key="tabbar"
         tabs
-        tabBarStyle={{ backgroundColor: '#FFFFFF', height: 70 }}
+        tabBarStyle={{ backgroundColor: '#1c1616', height: 50 }}
       >
         {/* MAIN */}
-        <Scene key="mainTab" title="Main" icon={TabIcon}>
+        <Scene key="mainTab" title="MAIN" icon={TabIcon}>
           <Scene
             key="main"
             component={Main}
-            title="main"
+            title="BLACKLIGHT"
+            color='#000000'
           />
         </Scene>
         {/* RECORD */}
-        <Scene key="recordTab" title="record" icon={TabIcon}>
+        <Scene key="recordTab" title="REC" icon={TabIcon}>
           <Scene
             key="recordHome"
             component={Record}
-            title=""
+            title="DROP PIN"
           />
           <Scene
             key="recordInput"
             component={RecordInput}
-            title=""
+            title="TYPE YOUR STORY"
           />
         </Scene>
         {/* NEARBY */}
-        <Scene key="nearbyTab" title="nearby" icon={TabIcon}>
+        <Scene key="nearbyTab" title="NEARBY" icon={TabIcon}>
           <Scene
             key="nearbyMem"
             component={NearbyMem}
-            title=""
+            title="MEMORY MAP"
           />
         </Scene>
       </Scene>
@@ -63,4 +84,31 @@ const RouterComponent = props => (
 );
 
 
+
+const styles = {
+  navBar: {
+    backgroundColor:'#000000',
+  },
+  navBarTitle: {
+    color:'#FFFFFF'
+  },
+  barButtonTextStyle: {
+    color:'#FFFFFF'
+  },
+  barButtonIconStyle: {
+    tintColor:'rgb(255,255,255)'
+  },
+}
+
 export default RouterComponent;
+
+
+
+// const TabIcon = ({ selected, title }) => {
+//   return (
+//     <Text style={{ color: selected ? 'red' : 'black' }}>{title}</Text>
+//   );
+// }
+
+
+// <Icon color="Red" name="home" size={34} type="home" />
--- react-native-app/components/RecordInput.js
+++ react-native-app/components/RecordInput.js
@@ -1,7 +1,7 @@
 
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { FormLabel, FormInput, Card, Button } from 'react-native-elements';
+import { FormLabel, FormInput, Button } from 'react-native-elements';
 import { View, Alert, TextInput } from 'react-native';
 import { Actions } from 'react-native-router-flux';
 
@@ -43,26 +43,34 @@ class RecordInput extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Card title="Enter your story">
           <FormLabel>Title</FormLabel>
-          <FormInput
+          <FormInput style={styles.input}
+            placeholer="Title"
+            color="#FFFFFF"
             onChangeText={this.handleTitle}
             value={this.state.title}
           />
-          <FormLabel>Text</FormLabel>
-          <TextInput
-            style={styles.textInput}
+          <FormLabel>YOUR STORY</FormLabel>
+
+          <TextInput style={styles.textInput}
+            placeholer="Your Story"
+            color="#FFFFFF"
             multiline
             onChangeText={this.handleText}
             value={this.state.text}
-          />
-          <Button
-            small
-            backgroundColor="#00BFFF"
-            onPress={this.handleSubmit}
-            title="submit"
-          />
-        </Card>
+        />
+
+          <View style={styles.buttonArea}>
+            <Button style={styles.buttonStyle}
+              small
+              title="SUBMIT"
+              backgroundColor="#ffffff"
+              icon={{name:'pencil', type: 'entypo', color: '#000000'}}
+              title="RECORD AT PIN"
+              color='#000000'
+              onPress={this.handleSubmit}
+            />
+          </View>
       </View>
     );
   }
@@ -70,15 +78,39 @@ class RecordInput extends Component {
 const styles = {
   container: {
     flex: 1,
-    backgroundColor: 'white',
+    backgroundColor: 'black',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20
   },
   textInput: {
-    height: 350, borderColor: 'gray', borderWidth: 1, fontSize: 20,
+    height: 270,
+    borderColor: 'gray',
+    // borderWidth: 0,
+    fontSize: 20,
+    color: '#ffffff',
+    padding: 10
   },
+  input: {
+    height: 40,
+    backgroundColor: 'rgba(192,192,192,0.3)',
+    marginBottom: 20,
+    // padding: 10
+  },
+  // buttonArea: {
+  //   flex: 1,
+  // },
+  buttonStyle: {
+    backgroundColor:'#000000',
+    padding: 10
+  }
 };
 
 
 const mapDispatch = { commitMemory };
 
 export default connect(null, mapDispatch)(RecordInput);
+
+
+
 
