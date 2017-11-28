4e605d64a7df1e2e72bf5922306dc0e785f7afc0
Trevor Storey <trevorstorey23@gmail.com>
2017-11-27 23:42:52 -0500

    [Feature-AR] seem to have got the distance tracking working

--- react-native-app/components/AR.js
+++ react-native-app/components/AR.js
@@ -5,6 +5,7 @@ import * as THREE from 'three';
 import ExpoTHREE from 'expo-three';
 import Expo from 'expo';
 import geolib from 'geolib';
+import { watchLocation, stopWatching } from '../store';
 
 class AR extends React.Component {
 
@@ -12,13 +13,37 @@ class AR extends React.Component {
     super(props);
     this.state = {
       memory: {
-        title: 'memory alta restaurant',
+        title: 'starbucks across the street',
         text: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?',
-        lng: -73.99820009999996,
-        lat: 40.7344108,
+        lng: -74.00882289999998,
+        lat: 40.70459220000001,
         authorId: 1,
       },
+      distance: NaN,
     };
+  }
+
+  componentDidMount() {
+    this.props.watchLocation();
+
+    setInterval(
+      this.performLocationChecking.bind(this),
+      1000);
+  }
+
+  componentWillUnmount() {
+    this.props.stopWatching();
+  }
+
+  performLocationChecking() {
+    let newDistance = geolib.getDistance(
+      { latitude: this.props.userCurrentLatitude, longitude: this.props.userCurrentLongitude },
+      { latitude: this.state.memory.lat, longitude: this.state.memory.lng },
+      10,
+      1
+    )
+    this.setState({ distance: newDistance });
+    console.log('\n', 'THIS.STATE.DISTANCE', this.state.distance, '\n')
   }
 
   _onGLContextCreate = async (gl) => {
@@ -69,4 +94,6 @@ const mapStateToProps = (state) => ({
   userCurrentLongitude: state.position.longitude,
 });
 
-export default connect(mapStateToProps)(AR);
+const mapDispatchToProps = { watchLocation, stopWatching };
+
+export default connect(mapStateToProps, mapDispatchToProps)(AR);
