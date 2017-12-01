80a19a73cabad029e77945c5e09f2a7b807ad448
Trevor Storey <trevorstorey23@gmail.com>
2017-12-01 05:17:55 -0500

    [Feature] made the app run much faster by preloading the assets(images and fonts)

--- App.js
+++ App.js
@@ -2,19 +2,63 @@
 import { View, StyleSheet } from 'react-native';
 import React, { Component } from 'react';
 import { Provider, connect } from 'react-redux';
-import store, { watchLocation, stopWatching } from './store';
+import store from './store';
 
+import { AppLoading, Asset, Font } from 'expo';
+import { Feather, MaterialIcons, FontAwesome, Entypo } from '@expo/vector-icons';
 
 import Router from './Router';
 
 export default class App extends Component {
+
+  state = {
+    isLoadingComplete: false,
+  };
+
+  _loadResourcesAsync = async () => {
+    return Promise.all([
+      Asset.loadAsync([
+        require('./images/wallpaper.jpg'),
+        require('./images/city.jpg'),
+        require('./images/blacklightIcon.png'),
+        require('./images/light.jpg'),
+      ]),
+      Font.loadAsync({
+        ...Ionicons.font,
+        ...Feather.font,
+        ...MaterialIcons.font,
+        ...FontAwesome.font,
+        ...Entypo.font,
+      }),
+    ]);
+  };
+
+  _handleLoadingError = error => {
+    console.warn(error);
+  };
+
+  _handleFinishLoading = () => {
+    this.setState({ isLoadingComplete: true });
+  };
+
+
   render() {
-    return (
-      <Provider store={store}>
-        <View style={{ flex: 1 }}>
-          <Router />
-        </View>
-      </Provider>
-    );
+    if (!this.state.isLoadingComplete) {
+      return (
+        <AppLoading
+          startAsync={this._loadResourcesAsync}
+          onError={this._handleLoadingError}
+          onFinish={this._handleFinishLoading}
+        />
+      );
+    } else {
+      return (
+        <Provider store={store}>
+          <View style={{ flex: 1 }}>
+            <Router />
+          </View>
+        </Provider>
+      );
+    }
   }
 }
