cd08e3238b685225917dd91307234eb7a510cc32
Alexander Tatarinov <tatarinov.alexander@gmail.com>
2017-12-06 16:01:13 -0500

    added promise.all to test rejections

--- api/tests/models.tests.js
+++ api/tests/models.tests.js
@@ -14,21 +14,71 @@ describe("page model", (done) => {
       })
   });
 
-  let page;
-  beforeEach(() => {
-    memory = Memory.build({
-      title: 'Geese',
-      text: 'Geese',
-      lng: 5,
-      lat: 5
-    })
-  });
 
-  // describe('route', () => {
-  //   it('returns url title prepended by /wiki/', () => {
-  //     expect(page.route).to.equal('/wiki/Geese')
-  //   })
-  // });
+
+    describe('create memory', () => {
+
+      let memory;
+      beforeEach(() => {
+        memory = Memory.build({
+          title: 'Geese',
+          text: 'Saw fat geese',
+          lng: 70,
+          lat: -47
+        });
+      });
+
+      it('saves title', () => {
+        expect(memory.title).to.equal('Geese');
+      });
+
+      it('saves title', () => {
+        expect(memory.text).to.equal('Saw fat geese');
+      });
+    });
+
+    describe('rejects empty entries', () => {
+      let mem1, mem2, mem3;
+      beforeEach(() => {
+        const m1 = Memory.build({
+          title: '1',
+          text: 'Some random memory text',
+          lng: 50,
+          lat: -55
+        });
+
+        const m2 = Memory.build({
+          title: 'Memory Title',
+          text: 'Some random memory text',
+          lng: undefined,
+          lat: -55
+        });
+
+        const m3 = Memory.build({
+          title: 'Memory Title',
+          text: 'Some random memory text',
+          lng: 70,
+          lat: undefined
+        });
+
+        return Promise.all([m1, m2, m3]).then(createdMemories => {
+          [mem1, mem2, mem3] = createdMemories;
+        });
+      });
+
+      it('saves title', () => {
+        expect(memory.title.length).to.equal(1);
+      });
+
+      // it('saves title', () => {
+      //   expect(memory.text).to.equal('Saw fat geese');
+      // });
+
+    });
+
+
+
+
 
   // describe('rendered content', () => {
   //   it('takes markdown content and converts to HTML', () => {
