ce4faa038435f6be185591d7a3d31699fabf6b8b
Trevor Storey <trevorstorey23@gmail.com>
2017-12-11 21:37:27 -0500

    [Tests] testing Login component inside of App. Still figuring out what appRenderer and appInstance are

--- react-native-app/__tests__/App.test.js
+++ react-native-app/__tests__/App.test.js
@@ -3,18 +3,29 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 
 import App from '../App';
+import { Button } from 'react-native-elements';
+
+// NOTE: I'm still figuring out what the difference is between using
+//   appRenderer vs appRenderer.getInstance() vs appInstance...
 
 describe('App component', () => {
 
   const appRenderer = renderer.create(<App />);
+  const appInstance = appRenderer.root;
   
   it('renders AppLoading component initially', () => {
     expect(appRenderer.toJSON()).toMatchSnapshot();
   });
   
   it('renders Login component after asset loading completes', () => {
+    // this calls _handleFinishLoading() on the root instance of App component.
     appRenderer.getInstance()._handleFinishLoading();
+
+    // appRenderer should now have rendered the Login component
     expect(appRenderer.toJSON()).toMatchSnapshot();
+
+    // appInstance is the Login component now. The Login component should have 3 buttons inside it.
+    expect(appInstance.findAllByType(Button).length).toEqual(3);
   });
 
 });
